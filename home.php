d28b9ec4dcfc76b3e27641ebb44ff8a0dc9ae266
Alla Belhafsi <alla.belhafsi@stagiaire-elan.fr>
2023-12-16 22:36:19 +0100

    completed

--- home.php
+++ home.php
@@ -19,56 +19,68 @@
     
     // Récupérez les informations de l'utilisateur depuis la session
     $subscriber = $_SESSION["subscriber"];
-    
+    $loginCount = $subscriber["login_count"];
     ?>
+
     <h1>Welcome to your Dashboard <?php echo $subscriber['pseudonym']; ?>!</h1>
-    <p>Email: <?php echo $subscriber['mail']; ?></p>
     
-    <!-- Autres informations de l'utilisateur à afficher -->
-    <!-- Affichage de la dernière connexion -->
     <?php
-    $lastLogin = new DateTime($subscriber['last_login']);
+    if($loginCount === null || $loginCount == 0) {  
+        ?>
+                
+        <p>
+            Félicitations <?php echo $subscriber['pseudonym']; ?>, c'est votre première connexion.
+        </p>
+        <p>
+            Bienvenue dans notre communauté dédiée aux NFT et au metaverse, <?php echo $subscriber['pseudonym']; ?>! Ensemble, explorons le potentiel infini de ce monde virtuel.
+        </p>
 
-    // Définir le fuseau horaire
-    $lastLogin->setTimezone(new DateTimeZone('Europe/Paris')); 
-
-    // Définir la localisation en français
-    setlocale(LC_TIME, 'fr_FR.utf8', 'fra'); 
-
-    // Créer un formateur de date pour le français
-    $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::NONE); 
-
-    // Formater la date avec la première lettre en majuscule pour le jour et le mois
-    $formattedDate = ucwords($formatter->format($lastLogin));
-
-    // Afficher l'heure au format souhaité
-    $time = $lastLogin->format('H\hi');
-    ?>
-    <p>Dernière connexion : <?php echo $formattedDate . " à " . $time; ?></p>
-    
-        <!-- Affichage de la dernière connexion -->
         <?php
-        if (isset($subscriber['last_logout'])) {
-            $lastLogout = new DateTime($subscriber['last_logout']);
-        
-            // Définir le fuseau horaire
-            $lastLogout->setTimezone(new DateTimeZone('Europe/Paris')); 
-        
-            // Définir la localisation en français
-            setlocale(LC_TIME, 'fr_FR.utf8', 'fra'); 
-        
-            // Créer un formateur de date pour le français
-            $formatterLogout = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::NONE); 
-        
-            // Formater la date avec la première lettre en majuscule pour le jour et le mois
-            $formattedDateLogout = ucwords($formatterLogout->format($lastLogout));
-        
-            // Afficher l'heure au format souhaité (14h04)
-            $timeLogout = $lastLogout->format('H\hi');
-            ?>
-            <p>Dernière déconnexion : <?php echo $formattedDateLogout . " à " . $timeLogout; ?></p>
-        
-        <?php } ?>
+    } else { 
+
+        //Autres informations de l'utilisateur à afficher
+        // Affichage de la dernière connexion 
+        $lastLogin = new DateTime($subscriber['last_login']);
+        // Définir le fuseau horaire
+        $lastLogin->setTimezone(new DateTimeZone('Europe/Paris')); 
+        // Définir la localisation en français
+        setlocale(LC_TIME, 'fr_FR.utf8', 'fra'); 
+        // Créer un formateur de date pour le français
+        $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::NONE); 
+        // Formater la date avec la première lettre en majuscule pour le jour et le mois
+        $formattedDate = ucwords($formatter->format($lastLogin));
+        // Afficher l'heure au format souhaité
+        $time = $lastLogin->format('H\hi');
+        ?>
+        <p>
+            Dernière connexion : <?php echo $formattedDate . " à " . $time; ?>
+        </p>
+        <?php
+        // Affichage de la dernière connexion 
+        $lastLogout = new DateTime($subscriber['last_logout']);
+    
+        // Définir le fuseau horaire
+        $lastLogout->setTimezone(new DateTimeZone('Europe/Paris')); 
+    
+        // Définir la localisation en français
+        setlocale(LC_TIME, 'fr_FR.utf8', 'fra'); 
+    
+        // Créer un formateur de date pour le français
+        $formatterLogout = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::NONE); 
+    
+        // Formater la date avec la première lettre en majuscule pour le jour et le mois
+        $formattedDateLogout = ucwords($formatterLogout->format($lastLogout));
+    
+        // Afficher l'heure au format souhaité (14h04)
+        $timeLogout = $lastLogout->format('H\hi');
+        $pseudo = $subscriber['pseudonym'];
+        ?>
+        <p>
+            Dernière déconnexion : <?php echo $formattedDateLogout . " à " . $timeLogout; ?>
+        </p> 
+
+    <?php } ?>
+       
 
     <!-- Liens vers d'autres fonctionnalités de l'application -->
     <p><a href="traitement.php?action=profile">Profile</a></p>
